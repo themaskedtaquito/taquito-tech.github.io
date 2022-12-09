fc01aad256631e514467784eee1e1bf74518b187
themaskedtaquito <mta305@nyu.edu>
2022-12-08 16:53:41 -0800

    Revert "Delete portfolio directory"
    This reverts commit fab3205dda0bde4086208f3f0cea05f3636da43f.

---
+++ portfolio/src/ProjectPage.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './ProjectPage.css';
+import ReactDOM from 'react-dom';
+import { Carousel } from 'react-responsive-carousel';
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Player } from 'video-react';
+import "video-react/dist/video-react.css";
+
+
+class ProjectPage extends Component {
+  render() {
+   
+
+    let imageReel = this.props.project.images.map(img=>{
+      return (
+        <div>
+          <img src = {img} />
+        </div>
+      )
+    });
+
+     if(this.props.project.gif){
+        imageReel.unshift(<div>
+          <img src = {this.props.project.gif}/>
+        </div>);
+      }
+
+      if(this.props.project.video){
+        imageReel.unshift(<div>
+          <Player
+            src={this.props.project.video}
+          />
+          
+        </div>);
+      }
+    return (
+
+      <div className="ProjectPage">
+        <Carousel className= "car" showThumbs = {false}  showStatus={false} infiniteLoop={true} width="600px">
+          {imageReel}
+        </Carousel>
+        
+        <div className="GRID">
+          <h1 className = "name">{this.props.project.name}</h1>
+
+          <div className = {this.props.project.type + " column"}>
+            <h2>Specs</h2>
+            <p>Platform: <span>{this.props.project.platform}</span></p>
+            <p>Tools: <span>{this.props.project.tools}</span></p>
+            <p>Language: <span>{this.props.project.language}</span></p>         
+            <p>Team Size: <span>{this.props.project.team}</span></p>
+            <p>Role: <span>{this.props.project.role}</span></p>
+          </div>
+
+          <div className = "details">
+            <h2>Overview</h2>
+            <div dangerouslySetInnerHTML={{ __html: this.props.project.description}} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default ProjectPage;
